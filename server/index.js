ad8c919d0633dcf4ee762bf45f019f25971d1c2c
ENGAGE\michael.brown <michael.brown@engagency.com>
2022-01-06 15:41:23 -0600

    adds more server logic

--- server/index.js
+++ server/index.js
@@ -14,13 +14,26 @@ app.use(express.json()); //req.body
 // Add bathroom
 app.post("/bathrooms", async(req, res) => {
     try {
-        const { description } = req.body;
+        const { name, description, confirmed_public, entry_key, requires_key, location  } = req.body;
         const newBathroom = await pool.query(
-            "INSERT INTO bathrooms (description) VALUES($1) RETURNING *",
-            [description]
+            "INSERT INTO bathrooms (name, description, confirmed_public, entry_key, requires_key, location) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+            [name, description, confirmed_public, entry_key, requires_key, location],
         );
         res.json(newBathroom.rows[0]);
+    } catch (err) { 
+        console.log(err.message);
+    }
+})
 
+// update a bathroom
+app.put("/bathrooms/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateBathroom = await pool.query("UPDATE bathrooms SET description = $1 WHERE bathroom_id = $2",
+        [description, id]
+        );
+        res.json("bathroom was updated");
     } catch (err) {
         console.log(err.message);
     }
@@ -30,10 +43,39 @@ app.post("/bathrooms", async(req, res) => {
 
 app.get("/bathrooms", async(req, res) => {
     try {
-        const allBathrooms = await pool.query("SELECT * FROM bathrooms");
+        const allBathrooms = await pool.query(
+            "SELECT * FROM bathrooms");
         res.json(allBathrooms.rows);
-    } catch (error) {
+    } catch (err) {
         console.log(err.message); 
+    }
+})
+
+// get bathroom 
+
+app.get("/bathrooms/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const bathroom = await pool.query(
+            "SELECT * FROM bathrooms WHERE bathroom_id = $1", [
+            id
+        ]);
+        res.json(bathroom.rows[0]);
+    } catch (err) {
+        console.log(err.message); 
+    }
+})
+
+// delete a bathroom 
+app.delete("/bathrooms/:id",  async(req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteBathroom = await pool.query("DELETE FROM bathrooms WHERE bathroom_id = $1", 
+        [id]
+        );
+        res.json("Bathroom was deleted");
+    } catch (err) {
+        console.log(err.message);   
     }
 })
 
